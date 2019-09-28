be31c4362e6db8c6f747861c2c7ca87729f7bbec
StolarQQ <stolar28@gmail.com>
2019-09-28 21:16:16 +0200

    Added temporal coupling example

---
+++ RefactorSchool/Temporal_Coupling/BakeGood.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RefactorSchool.Temporal_Coupling
+{
+    public class BakeGood : BakedGoodBase
+    {
+        public void MakeBakeGood()
+        {
+            MakeBakeTemplate();
+        }
+
+        protected override void PrepareCrust()
+        {
+            Console.WriteLine("PrepareCrust");
+        }
+
+        protected override void AddToppings()
+        {
+            Console.WriteLine("AddToppings");
+        }
+
+        protected override void Bake()
+        {
+            Console.WriteLine("Bake");
+        }
+
+        protected override void CutIntoSlices()
+        {
+            Console.WriteLine("CutIntoSlices");
+            Console.WriteLine("Good job, bake is done !");
+        }
+    }
+}
---
+++ RefactorSchool/Temporal_Coupling/BakedGoodBase.cs
@@ -0,0 +1,18 @@
+﻿namespace RefactorSchool
+{
+    public abstract class BakedGoodBase
+    {
+        public void MakeBakeTemplate()
+        {
+            PrepareCrust();
+            AddToppings();
+            Bake();
+            CutIntoSlices();
+        }
+
+        protected abstract void PrepareCrust();
+        protected abstract void AddToppings();
+        protected abstract void Bake();
+        protected abstract void CutIntoSlices();
+    }
+}
