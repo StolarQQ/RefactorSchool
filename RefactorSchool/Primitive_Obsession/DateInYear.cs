7b135ce0176ab49459377c3b108a20e42fcf6aa5
StolarQQ <stolar28@gmail.com>
2019-09-28 21:16:00 +0200

    Added primitive obsession example

---
+++ RefactorSchool/Primitive_Obsession/DateInYear.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace RefactorSchool.Primitive_Obsession
+{
+    class DateInYear
+    {
+        public Guid Id { get; private set; }
+        public int Month { get; private set; }
+        public int Day { get; private set; }
+        public string Description { get; private set; }
+
+        public DateInYear(int month, int day, string description)
+        {
+            Id = Guid.NewGuid();
+            SetMonth(month);
+            SetDay(day);
+            Description = description;
+        }
+
+        public void SetMonth(int month)
+        {
+            if (month > 12)
+            {
+                throw new ArgumentException("Month value cannot be grater than 12");
+            }
+
+            if (Month == month)
+            {
+                return;
+            }
+
+            Month = month;
+        }
+
+        public void SetDay(int day)
+        {
+            // ;)
+            if (day > 31)
+            {
+                throw new ArgumentException("Day value cannot be grater than 31");
+            }
+
+            if (Day == day)
+            {
+                return;
+            }
+
+            Day = day;
+        }
+
+    }
+}
---
+++ RefactorSchool/Primitive_Obsession/HolidayStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RefactorSchool.Primitive_Obsession
+{
+    internal static class HolidayStore
+    {
+        private static ISet<DateInYear> _holidayDates = new HashSet<DateInYear>();
+        public static IEnumerable<DateInYear> HolidayDates => _holidayDates;
+        public static void AddHoliday(DateInYear date)
+        {
+            _holidayDates.Add(date);
+        }
+
+        public static void AddBadHoliday(int i, int i1)
+        {
+            // Just bad example of code
+            throw new NotImplementedException();
+        }
+    }
+}
